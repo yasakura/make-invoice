79792f276fc347a46285ae9fd582407e7a167bf6
yasakura <freelance@ljamal.net>
2016-04-30 13:16:03 +0200

    add files for generate invoice

---
+++ generate_invoice.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function(){
+  var handlebars = require('handlebars')
+    , fs = require('fs');
+
+  fs.readFile('./template/invoice_ae.hbs', 'utf8', function(err, data) {
+    if (err) throw err;
+    writeFile(data);
+  });
+
+  function writeFile(data) {
+    fs.writeFile('./template/invoice.html', compileHandlebars(data), function(err){
+      if (err) throw err;
+    });
+  }
+
+  function compileHandlebars(data) {
+    var json = require('./data.json')
+      , template = handlebars.compile(data.toString())
+      , outputString = template(json);
+
+    return outputString;
+  }
+})();
+
+
+// faire package.json pour installer dependances lors du clone du dépot avec npm i
